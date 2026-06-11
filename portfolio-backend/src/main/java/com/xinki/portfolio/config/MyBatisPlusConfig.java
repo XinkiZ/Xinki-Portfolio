c5fb44ee9c3f6a35c29f11e64273d6f73d1f49b5
XinkiZ <1918211484@qq.com>
2026-06-11 21:28:53 +0800

    fix: add BeanFactoryPostProcessor to fix MyBatis-Plus 3.5.5 + Spring Boot 3.2 compatibility
    Remove invalid 'factoryBeanObjectType' attribute from mapper bean definitions

--- portfolio-backend/src/main/java/com/xinki/portfolio/config/MyBatisPlusConfig.java
+++ portfolio-backend/src/main/java/com/xinki/portfolio/config/MyBatisPlusConfig.java
@@ -6,8 +6,35 @@ import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerIntercept
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class MyBatisPlusConfig {
+
+    /** Fix MyBatis-Plus 3.5.5 compatibility with Spring Boot 3.2+: remove invalid factoryBeanObjectType. */
+    @Bean
+    public static BeanDefinitionRegistryPostProcessor mybatisMapperFixer() {
+        return new BeanDefinitionRegistryPostProcessor() {
+            @Override
+            public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+                for (String name : registry.getBeanDefinitionNames()) {
+                    BeanDefinition bd = registry.getBeanDefinition(name);
+                    if (bd.hasAttribute("factoryBeanObjectType")) {
+                        bd.removeAttribute("factoryBeanObjectType");
+                    }
+                }
+            }
+
+            @Override
+            public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {}
+        };
+    }
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
